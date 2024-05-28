6b952c812bdb97b7a52633713780042b1c5b712d
Nirbheek Setia <nirbheek.setia@tum.de>
2024-05-28 18:47:40 +0200

    Changes: Nirbheek 28.05

--- alg.cpp
+++ alg.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <unordered_map>
 #include <utility>
 #include <tuple>
 #include "API.h"
@@ -37,11 +38,20 @@ bool isValid(int x, int y, int prevC, int newC) {
 }
 
 // flood fill using BFS
-void floodFill(int x, int y, int prevC, int newC) {
+void floodFill(int startx, int starty,int endx,int endy, int prevC, int newC) {
     std::queue<std::pair<int, int>> queue;
-    queue.push(std::make_pair(x, y));
+    queue.push(std::make_pair(startx, starty));
 
-    API::setColor(x, y, newC);
+
+    // keep track of direction the rpobot is looking in using int variable
+    //0=er schaut nach oben, 1=er schaut nach rechts, 2=erschaut nach unten, 3=er schaut nach links
+    int direction = 0;
+    
+    //keep track of previous position
+    int posX_prev = 0;
+    int posY_prev = 0;
+
+    API::setColor(startx, starty, newC);
 
     while (!queue.empty()) {
         std::pair<int, int> currentPixel = queue.front();
@@ -50,26 +60,138 @@ void floodFill(int x, int y, int prevC, int newC) {
         int posX = currentPixel.first;
         int posY = currentPixel.second;
 
+        API::setColor(posX, posY, newC);
+
+        if(!(posX_prev==0 && posY_prev==0)){
+
+            //Difference of coordinates 
+            int diffx=posX-posX_prev;
+            int diffy=posY-posY_prev;
+
+            //This switch decides whether the robot can move to currentPixel and if yes the switch moves it
+            switch (direction)
+            {
+            case 0:
+                if(diffx==0 && diffy==1 && !API::wallFront()){
+                    API::moveForward();
+                }
+                else if(diffx==1 && diffy==0 && !API::wallRight()){
+                    API::turnRight();
+                    API::moveForward();
+                    direction=1;
+                }
+                else if(diffx==-1 && diffy==0 && !API::wallLeft()){
+                    API::turnLeft();
+                    API::moveForward();
+                    direction=3;
+                }
+                else if(diffx==0 && diffy==-1){
+                    API::turnRight();
+                    API::turnRight();
+                    API::moveForward();
+                    direction=2;
+                }
+                break;
+            case 1:
+                if(diffx==1 && diffy==0 && !API::wallFront()){
+                    API::moveForward();
+                }
+                else if(diffx==0 && diffy==-1 && !API::wallRight()){
+                    API::turnRight();
+                    API::moveForward();
+                    direction=2;
+                }
+                else if(diffx==0 && diffy==1 && !API::wallLeft()){
+                    API::turnLeft();
+                    API::moveForward();
+                    direction=0;
+                }
+                else if(diffx==-1 && diffy==0){
+                    API::turnRight();
+                    API::turnRight();
+                    API::moveForward();
+                    direction=3;
+                }
+                break;
+            case 2:
+                if(diffx==0 && diffy==-1 && !API::wallFront()){
+                    API::moveForward();
+                }
+                else if(diffx==-1 && diffy==0 && !API::wallRight()){
+                    API::turnRight();
+                    API::moveForward();
+                    direction=3;
+                }
+                else if(diffx==1 && diffy==0 && !API::wallLeft()){
+                    API::turnLeft();
+                    API::moveForward();
+                    direction=1;
+                }
+                else if(diffx==0 && diffy==1){
+                    API::turnRight();
+                    API::turnRight();
+                    API::moveForward();
+                    direction=0;
+                }
+                break;
+            case 3:
+                if(diffx==-1 && diffy==0 && !API::wallFront()){
+                    API::moveForward();
+                }
+                else if(diffx==0 && diffy==1 && !API::wallRight()){
+                    API::turnRight();
+                    API::moveForward();
+                    direction=0;
+                }
+                else if(diffx==0 && diffy==-1 && !API::wallLeft()){
+                    API::turnLeft();
+                    API::moveForward();
+                    direction=2;
+                }
+                else if(diffx==1 && diffy==0){
+                    API::turnRight();
+                    API::turnRight();
+                    API::moveForward();
+                    direction=1;
+                }
+                break;
+            }
+        }
+
+        //check if end is reached
+        if(currentPixel == std::make_pair(endx,endy)){
+            API::setColor(endx, endy, newC);
+            API::ackReset();
+            std::cout << "Path found" << std::endl;
+            return;
+        }
+        
+
+
         // check the neighbors
-        if (isValid(posX + 1, posY, prevC, newC)) {
-            API::setColor(posX + 1, posY, newC);
+        // setcolor not necessary, I moved it to the top(s. line 63)
+        if (isValid(posX + 1, posY, prevC, newC) && ) {
+            //API::setColor(posX + 1, posY, newC);
             queue.push(std::make_pair(posX + 1, posY));
         }
 
         if (isValid(posX - 1, posY, prevC, newC)) {
-            API::setColor(posX - 1, posY, newC);
+            //API::setColor(posX - 1, posY, newC);
             queue.push(std::make_pair(posX - 1, posY));
         }
 
         if (isValid(posX, posY + 1, prevC, newC)) {
-            API::setColor(posX, posY + 1, newC);
+            //API::setColor(posX, posY + 1, newC);
             queue.push(std::make_pair(posX, posY + 1));
         }
 
         if (isValid(posX, posY - 1, prevC, newC)) {
-            API::setColor(posX, posY - 1, newC);
+            //API::setColor(posX, posY - 1, newC);
             queue.push(std::make_pair(posX, posY - 1));
         }
+
+        posX_prev = posX;
+        posY_prev = posY;
     }
 }
 
@@ -79,15 +201,12 @@ int main() {
     int prevC = API::getColor(startX, startY);
     int newC = 3;
 
-    
-    floodFill(startX, startY, prevC, newC);
+    //Adjust the values accordingly
+    int endX=15, endY=15;
 
-    for (int x = 0; x < API::mazeWidth(); ++x) {
-        for (int y = 0; y < API::mazeHeight(); ++y) {
-            std::cout << API::getColor(x, y) << " ";
-        }
-        std::cout << std::endl;
-    }
+    
+    floodFill(startX, startY,endX,endY, prevC, newC);
+
 
     return 0;
 }
